7f350ae9c00b37f4bdc8d904965e6c6e8b420b57
asdr the admin <admin-o@asdr0.(none)>
2010-12-29 21:07:01 +0200

    fix

--- dsgner.lisp
+++ dsgner.lisp
@@ -21,78 +21,79 @@
 
 (in-package :dsgner)
 
-(defparameter *tag-string* (make-array '(0) 
-				       :element-type 'base-char
-				       :fill-pointer 0
-				       :adjustable t))
+(defun empty-string ()
+  (make-array '(0) 
+              :element-type 'base-char
+              :fill-pointer 0
+              :adjustable t))
+
+(defparameter *tag-string* (empty-string))
+ 
 (defun clean-output-buffer ()
-  (setf *tag-string* (make-array '(0) 
-				 :element-type 'base-char
-				 :fill-pointer 0
-				 :adjustable t)))
-
-(defmacro html (&rest body)
-  `(with-use-tag (html) 
-       ()
-     ,@body))
-
-(defmacro head (&rest body)
-  `(with-use-tag (head) 
-       ()
-     ,@body))
-
-(defmacro title (title)
-  `(with-use-tag (title) 
-       ()
-     ,title))
-
-(defmacro body ((&optional &rest attribs) &body body) 
-  `(with-use-tag (body)
-     ,attribs
-     ,@body))
-
+  (setf *tag-string* (empty-string)))
 
 
 (defmacro with-use-tag ((tag) (&optional &rest attributes) &body body)
   (let ((strsym (gensym))
-	(attsym (gensym))
-	(len-attsym (gensym))
-	(csym (gensym))
-	(bodysym (gensym)))
-    `(let* ((,attsym ',attributes)
-	    (,len-attsym (length ,attsym))
-	    (,strsym *tag-string*);variable capture
-	    (,bodysym ',body))
+        (attsym (gensym))
+        (len-attsym (gensym))
+        (csym (gensym))
+        (bodysym (gensym))
+        (tag-name (string-downcase (string tag))))
+    `(let* (,@(unless (null attributes)
+               (list
+                `(,attsym ',attributes)
+                `(,len-attsym (length ,attsym))))
+            (,strsym *tag-string*);variable capture
+            (,bodysym ',body))
        (with-output-to-string (s ,strsym)
-	 (if (not (null ,attsym))
-	     (progn
-	       (format s "<~A" ',tag)
-	       (do ((,csym 0 (+ ,csym 2)))
-		   ((>= ,csym ,len-attsym))
-		 (format s " ~A=\"~A\"" (first ,attsym) (second ,attsym))
-		 (setf ,attsym (cddr ,attsym))))
-	     (format s "<~A" ',tag))
-	 
-	 ;;simdi body kismindaki kod icin biseyler yapmak lazim
-	 (cond ((null ,bodysym)
-		(format s " />"))
-	       ((and (= (length ,bodysym) 1)
-		     (atom (car ,bodysym)))
-		(format s ">~A" (car ,bodysym))
-		(format s "</~A>" ',tag))
-	       (t (format s ">") 
-		  ,@body
-		  (format s "</~A>" ',tag)))))))
+         ,@(if (not (null attributes))
+               (list
+                `(format s "<~A" ,tag-name)
+                `(do ((,csym 0 (+ ,csym 2)))
+                     ((>= ,csym ,len-attsym))
+                   (format s " ~A=\"~A\"" (string-downcase (string (car ,attsym))) (cadr ,attsym))
+                   (setf ,attsym (cddr ,attsym))))
+               (list 
+                     `(format s "<~A" ,tag-name)))
+         
+         ;;simdi body kismindaki kod icin biseyler yapmak lazim
+         (cond ((null ,bodysym)
+                (format s " />"))
+           ((and (= (length ,bodysym) 1)
+                 (atom (car ,bodysym)))
+            (format s ">~A" (car ,bodysym))
+            (format s "</~A>" ,tag-name))
+           (t (format s ">") 
+              ,@body
+              (format s "</~A>" ,tag-name)))))))
+
+(defmacro deftag (tag)
+  `(defmacro ,tag ((&optional &rest attribs) &body body)
+     `(with-use-tag (,',tag)
+          ,attribs
+        ,@body)))
+
+(defmacro deftags (&rest tags)
+  `(progn
+     ,@(mapcar #'(lambda(tag)
+                   `(deftag ,tag))
+               tags)))
+
+;;define tags of HTML 4.01 / XHTML 1.0
+(deftags :!DOCTYPE :a :abbr :acronym :address :applet :b :base :basefont :bdo :big :blockquote :body :br :button :caption :center
+  :cite :code :col :colgroup :dd :del :dfn :dir :div :dl :dt :em :fieldset :font :form :frame :frameset :h1 :h2 :h3 :h4 :h5 :h6 :head :hr
+  :html :i :iframe :img :input :ins :isindex :kbd :label :legend :li :link :map :menu :meta :noframes :noscript :object :ol :optgroup
+  :option :p :param :pre :q :s :samp :script :select :small :span :strike :strong :style :sub :sup :table :tbody :td :textarea :tfoot
+  :th :thead :title :tr :tt :u :ul :var :xmp)
+
 
 (defun test1 ()
-  (princ 
-   (let ((*tag-string* (make-array '(0) 
-				   :element-type 'base-char
-				   :fill-pointer 0
-				   :adjustable t)))
-     (html
-      (head
-       (title "ASDR"))
-      (body (:color "Green"
-	     :onload "javascript: alert('asdr!');")))
-     *tag-string*)))
+  (let ((*tag-string* (empty-string)))
+    (:div (:class "x-tree-root-node-ct")
+      (:li (:class "x-tree-root-node")
+        (:div ())
+        (:ul ()
+          (:li ()
+            (:img (:src "icons/xxx.png"))))))
+    (princ *tag-string*)))
