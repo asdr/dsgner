c8eae571e306a524062eec9bf4f013f5a7f5f6d9
asdr the admin <admin-o@asdr0.(none)>
2010-12-30 22:13:25 +0200

    .

--- dsgner.lisp
+++ dsgner.lisp
@@ -6,42 +6,54 @@
               :fill-pointer 0
               :adjustable t))
 
+(defmacro nif (test then &optional else)
+  `(if (not ,test)
+       ,then
+       ,else))
+
+(defun format-tag (stream tag attributes)
+  (let ((output (format stream "<~A" tag)))
+    (when attributes
+      (do ((i 0 (+ i 2))
+           (att attributes (cddr att))
+           (len (length attributes)))
+          ((>= i len))
+        (concatenate 'string 
+                     output
+                     (format stream
+                             " ~A=\"~A\""
+                             (string-downcase (string (car att)))
+                             (cadr att)))))
+    output))
+      
 (defmacro with-use-tag ((tag) (&optional &rest attributes) &body body)
   (let ((strsym (gensym))
         (attsym (gensym))
-        (len-attsym (gensym))
-        (csym (gensym))
-	(tagsym (gensym))
+        (tagsym (gensym))
         (tag-name (string-downcase (string tag))))
-    `(let* (,@(unless (null attributes)
-		      (list `(,attsym ',attributes)
-			    `(,len-attsym (length ,attsym))))
-            (,strsym (empty-string)))
+    `(let (,@(nif (null attributes)
+               (list `(,attsym ',attributes)))
+             (,strsym (empty-string)))
        (with-output-to-string (,tagsym ,strsym)
-         ,@(if (not (null attributes))
-               (list `(format ,tagsym "<~A" ,tag-name)
-		     `(do ((,csym 0 (+ ,csym 2)))
-			  ((>= ,csym ,len-attsym))
-			(format ,tagsym 
-				" ~A=\"~A\"" 
-				(string-downcase (string (car ,attsym))) 
-				(cadr ,attsym))
-			(setf ,attsym (cddr ,attsym))))
-	       (list `(format ,tagsym "<~A" ,tag-name)))
          
-         ;;simdi body kismindaki kod icin biseyler yapmak lazim
-         ,@(cond ((null body)
-		  (list `(format ,tagsym " />")))
-		 ((and (= (length body) 1)
-		       (atom (car body)))
-		  (list `(format ,tagsym ">~A" ,(car body))
-			`(format ,tagsym "</~A>" ,tag-name)))
-		 (t (append (list `(format ,tagsym ">")) 
-			    (mapcar #'(lambda (f) 
-					`(format ,tagsym "~A" ,f))
-				    body)
-			    (list `(format ,tagsym "</~A>" ,tag-name))))))
-       ,strsym)))
+         ;;handle attributes
+         ,(nif (null attributes)
+            `(format-tag ,tagsym ,tag-name ,attsym)
+            `(format-tag ,tagsym ,tag-name nil))
+       
+       ;;handle BODY part
+       ,@(cond ((null body)
+                (list `(format ,tagsym " />")))
+           ((and (= (length body) 1)
+                 (atom (car body)))
+            (list `(format ,tagsym ">~A" ,(car body))
+                  `(format ,tagsym "</~A>" ,tag-name)))
+           (t (append (list `(format ,tagsym ">")) 
+                      (mapcar #'(lambda (f) 
+                                  `(format ,tagsym "~A" ,f))
+                              body)
+                      (list `(format ,tagsym "</~A>" ,tag-name))))))
+    ,strsym)))
 
 (defmacro deftag (tag)
   `(defmacro ,tag ((&optional &rest attribs) &body body)
@@ -57,20 +69,37 @@
 
 ;;define tags of HTML 4.01 / XHTML 1.0
 (deftags :!DOCTYPE :a :abbr :acronym :address :applet :b :base :basefont 
-	 :bdo :big :blockquote :body :br :button :caption :center :cite 
-	 :code :col :colgroup :dd :del :dfn :dir :div :dl :dt :em :fieldset 
-	 :font :form :frame :frameset :h1 :h2 :h3 :h4 :h5 :h6 :head :hr
-	 :html :i :iframe :img :input :ins :isindex :kbd :label :legend :li 
-	 :link :map :menu :meta :noframes :noscript :object :ol :optgroup
-	 :option :p :param :pre :q :s :samp :script :select :small :span 
-	 :strike :strong :style :sub :sup :table :tbody :td :textarea :tfoot
-	 :th :thead :title :tr :tt :u :ul :var :xmp)
+	  :bdo :big :blockquote :body :br :button :caption :center :cite 
+	   :code :col :colgroup :dd :del :dfn :dir :div :dl :dt :em :fieldset 
+	    :font :form :frame :frameset :h1 :h2 :h3 :h4 :h5 :h6 :head :hr
+	     :html :i :iframe :img :input :ins :isindex :kbd :label :legend :li 
+	      :link :map :menu :meta :noframes :noscript :object :ol :optgroup
+	       :option :p :param :pre :q :s :samp :script :select :small :span 
+	        :strike :strong :style :sub :sup :table :tbody :td :textarea :tfoot
+		 :th :thead :title :tr :tt :u :ul :var :xmp)
 
 
 (defun test1 ()
   (:div (:class "x-tree-root-node-ct")
 	(:li (:class "x-tree-root-node")
-	     (:div ())
-	     (:ul ()
-		  (:li ()
-		       (:img (:src "icons/xxx.png")))))))
+	          (:div ())
+		       (:ul ()
+			      (:li ()
+				          (:img (:src "icons/xxx.png")))))))
+
+(defun test2 ()
+  (:html (:xmlns "http://www.w3.org/1999/xhtml"
+          :xml\:lang "en")
+    (:head ()
+      (:meta (:http-equiv "Content-Type"
+              :content "text/html;charset=utf-8"))
+      (:title ()
+        "Web Page")
+      (:link (:type "text/css"
+              :rel "stylesheet"
+              :href "/general.css")))
+    (:body ()
+      (:div (:id "header")
+        (:p ()
+          (:h1 ()
+            "HEADER..."))))))
